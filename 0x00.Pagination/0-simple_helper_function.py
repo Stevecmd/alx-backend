d96367c10787f1537a7089a747564ba5d8e6bc5d
SteveCmd <murimi101@gmail.com>
2024-10-25 13:59:20 +0300

    Add description to header and shebang line

--- 0x00.Pagination/0-simple_helper_function.py
+++ 0x00.Pagination/0-simple_helper_function.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""Pagination helper function.
+"""
+
+
 def index_range(page: int, page_size: int) -> tuple:
     """Return a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return in a list for those

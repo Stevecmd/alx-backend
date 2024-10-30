103e326d55c04c9a4785f8deb09215924a98b32c
SteveCmd <murimi101@gmail.com>
2024-10-30 11:23:00 +0300

    Add LFUCache implementation: the LFUCache class inherits from BaseCaching and implements a Least Frequently Used (LFU) caching system

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" LFUCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """ LFUCache defines a LFU caching system """
+
+    def __init__(self):
+        """ Initialize the class """
+        super().__init__()
+        self.frequency = {}
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.order.remove(key)
+            self.cache_data[key] = item
+            self.order.append(key)
+            if key in self.frequency:
+                self.frequency[key] += 1
+            else:
+                self.frequency[key] = 1
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                min_freq = min(self.frequency.values())
+                lfu_keys = [
+                    k for k, v in self.frequency.items()
+                    if v == min_freq
+                ]
+                if len(lfu_keys) > 1:
+                    lru_key = min(lfu_keys, key=lambda k: self.order.index(k))
+                else:
+                    lru_key = lfu_keys[0]
+                del self.cache_data[lru_key]
+                del self.frequency[lru_key]
+                self.order.remove(lru_key)
+                print("DISCARD: {}".format(lru_key))
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        self.frequency[key] += 1
+        self.order.remove(key)
+        self.order.append(key)
+        return self.cache_data.get(key)

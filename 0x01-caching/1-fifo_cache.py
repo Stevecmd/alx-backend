a46853375f7853ec6fed1c92a9cfac7363bb9feb
SteveCmd <murimi101@gmail.com>
2024-10-30 11:09:11 +0300

    Add FIFO cache implementation
    This commit adds a new file `1-fifo_cache.py` which contains the implementation of a First-In-First-Out (FIFO) caching system. The `FIFOCache` class inherits from the `BaseCaching` class and includes methods for adding items to the cache (`put`) and retrieving items from the cache (`get`). The cache uses a list called `order` to keep track of the order in which items were added, and if the cache exceeds its maximum capacity, the oldest item is discarded.

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" FIFOCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache defines a FIFO caching system """
+
+    def __init__(self):
+        """ Initialize the class """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.order.remove(key)
+            self.cache_data[key] = item
+            self.order.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key = self.order.pop(0)
+                del self.cache_data[first_key]
+                print("DISCARD: {}".format(first_key))
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)

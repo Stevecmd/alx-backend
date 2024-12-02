a17bf82154f191c71e23ba10cda4584f1bb82b63
SteveCmd <murimi101@gmail.com>
2024-12-02 20:25:26 +0300

    8. Track progress and errors with Kue: Create the Job creator: Implement job processing for notification jobs using Kue

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -1,3 +1,8 @@
+// Create the Job processor
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
 const jobs = [
   {
     phoneNumber: '4153518780',
@@ -44,3 +49,20 @@ const jobs = [
     message: 'This is the code 4321 to verify your account'
   }
 ];
+
+jobs.forEach((jobData) => {
+  const job = queue.create('push_notification_code_2', jobData)
+    .save((err) => {
+      if (!err) {
+        console.log(`Notification job created: ${job.id}`);
+      }
+    });
+
+  job.on('complete', () => {
+    console.log(`Notification job ${job.id} completed`);
+  }).on('failed', (err) => {
+    console.log(`Notification job ${job.id} failed: ${err}`);
+  }).on('progress', (progress) => {
+    console.log(`Notification job ${job.id} ${progress}% complete`);
+  });
+});

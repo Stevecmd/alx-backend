324fb1a6a67909695680d7cb7bd6d685d4127a14
SteveCmd <murimi101@gmail.com>
2024-12-02 15:45:00 +0300

    2. Node Redis client and basic operations:
    Add Redis operations for setting and retrieving school values

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,34 @@
+// Connect to the Redis server and log appropriate
+// messages based on the connection status
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Function to set a new school in Redis
+function setNewSchool (schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+// Function to display the value of a school from Redis
+function displaySchoolValue (schoolName) {
+  client.get(schoolName, (err, reply) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(reply);
+    }
+  });
+}
+
+// Function calls
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
